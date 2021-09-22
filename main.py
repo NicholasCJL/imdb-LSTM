efdb92ac54f307f7a2e19e984fa4228595891672
Nicholas <chong.jiale.nicholas@gmail.com>
2021-09-22 16:58:30 +0800

    Added fitting and callbacks.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tensorflow as tf
-from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.callbacks import ModelCheckpoint, CSVLogger
 import data_processing as dp
 import rnn_model
 
@@ -10,6 +9,9 @@ HIDDEN_SIZE = 64
 EMBED_SIZE = 128
 NUM_EPOCHS = 10
 INPUT_SIZE = 100
+BATCH_SIZE = 128
+
+model_name = "LSTM64_D1"
 
 # obtain data
 dataset = dp.DataSet(30000, maxlen=INPUT_SIZE, train_portion=0.8)
@@ -20,5 +22,15 @@ vocab_size = dataset.get_vocab_length()
 model = rnn_model.get_model(vocab_size, EMBED_SIZE, INPUT_SIZE, HIDDEN_SIZE)
 
 # checkpoint location
-filepath = "model/LSTM64_D1_weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
+filepath = "model/{model_name}/weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=False, mode='min')
+time_history = rnn_model.TimeHistory()
+csv_logger = CSVLogger("model/{model_name}/training.csv")
+callbacks_list = [checkpoint, time_history, csv_logger]
+
+# fitting
+model.fit(x_train, y_train,
+          epochs=NUM_EPOCHS,
+          batch_size=BATCH_SIZE,
+          validation_data=(x_test, y_test),
+          callbacks=callbacks_list)
