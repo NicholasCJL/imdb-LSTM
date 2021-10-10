7f4b75ea958b186eb35c344be66b3702cc9e41c1
Nicholas <chong.jiale.nicholas@gmail.com>
2021-10-10 16:40:16 +0800

    Added hypertuner

--- main.py
+++ main.py
@@ -1,42 +1,77 @@
 import tensorflow as tf
-from tensorflow.keras.callbacks import ModelCheckpoint, CSVLogger
+from tensorflow.keras.callbacks import ModelCheckpoint, CSVLogger, EarlyStopping
 import data_processing as dp
 import rnn_model
+from keras_tuner import BayesianOptimization
 
 # remove GPU from tensorflow visibility, set device to CPU (use for small networks)
 # tf.config.set_visible_devices([], 'GPU')
 
-HIDDEN_SIZE = 128
-EMBED_SIZE = 128
-NUM_EPOCHS = 50
+NUM_EPOCHS = 35
 INPUT_SIZE = 80
 BATCH_SIZE = 128
-DROPOUT = 0.3
-REC_DROPOUT = 0.0
+VOCAB_SIZE = 4000
 
-# {embed_dims}_LSTM{layer_size}-dropout-rec_dropout_{dense layers}
-model_name = "128_LSTM128-0.3-0.0_D64s_D1"
+model_name = "bayesian"
 
 # obtain data
-dataset = dp.DataSet(25000, maxlen=INPUT_SIZE, train_portion=0.8)
+dataset = dp.DataSet(VOCAB_SIZE, maxlen=INPUT_SIZE, train_portion=0.7)
 (x_train, y_train), (x_test, y_test) = dataset.get_data()
 vocab_size = dataset.get_vocab_length()
 
-# get model
-model = rnn_model.get_model(vocab_size, EMBED_SIZE, INPUT_SIZE, HIDDEN_SIZE,
-                            dropout=DROPOUT,
-                            recurrent_dropout=REC_DROPOUT)
+# tuning
+tuner = BayesianOptimization(
+    rnn_model.get_tuned_model,
+    objective="val_accuracy",
+    max_trials=20,
+    executions_per_trial=3,
+    overwrite=False,
+    directory=f'model/{model_name}',
+    project_name="imdbLSTM"
+)
 
-# checkpoint location
-filepath = f"model/{model_name}/weights-improvement-{{epoch:03d}}-{{val_loss:.4f}}-{{val_accuracy:.4f}}.hdf5"
-checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
-time_history = rnn_model.TimeHistory()
+print(tuner.search_space_summary())
+
 csv_logger = CSVLogger(f"model/{model_name}/training.csv")
-callbacks_list = [checkpoint, time_history, csv_logger]
+early_stopping = EarlyStopping(
+    monitor="val_accuracy",
+    patience=5,
+    restore_best_weights=True
+)
+callbacks_list = [csv_logger, early_stopping]
 
-# fitting
+tuner.search(x_train, y_train,
+             epochs=NUM_EPOCHS,
+             batch_size=BATCH_SIZE,
+             validation_data=(x_test, y_test),
+             callbacks=callbacks_list)
+
+# get best hyperparams and retrain model
+hyperparams = tuner.get_best_hyperparameters()[0]
+print(hyperparams.get('units'), hyperparams.get('learning_rate'))
+model = tuner.hypermodel.build(hyperparams)
+model.summary()
+filepath = f"model/{model_name}/weights-improvement-{{epoch:03d}}-{{val_loss:.4f}}-{{val_accuracy:.4f}}.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='val_accuracy', verbose=1, save_best_only=True, mode='max')
+callbacks_list = [checkpoint, csv_logger]
 model.fit(x_train, y_train,
           epochs=NUM_EPOCHS,
           batch_size=BATCH_SIZE,
           validation_data=(x_test, y_test),
           callbacks=callbacks_list)
+# model.save(f"model/{model_name}/best_model.hdf5")
+
+
+# # checkpoint location
+# filepath = f"model/{model_name}/weights-improvement-{{epoch:03d}}-{{val_loss:.4f}}-{{val_accuracy:.4f}}.hdf5"
+# checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+# time_history = rnn_model.TimeHistory()
+# csv_logger = CSVLogger(f"model/{model_name}/training.csv")
+# callbacks_list = [checkpoint, time_history, csv_logger]
+#
+# # fitting
+# model.fit(x_train, y_train,
+#           epochs=NUM_EPOCHS,
+#           batch_size=BATCH_SIZE,
+#           validation_data=(x_test, y_test),
+#           callbacks=callbacks_list)
--- rnn_model.py
+++ rnn_model.py
@@ -1,7 +1,35 @@
 import time
+from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import Callback
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Embedding
+
+HIDDEN_SIZE = 64
+EMBED_SIZE = 32
+INPUT_SIZE = 80
+DROPOUT = 0.2
+REC_DROPOUT = 0.0
+VOCAB_SIZE = 4000
+
+# model tuner
+def get_tuned_model(hp):
+    model = Sequential()
+    model.add(Embedding(VOCAB_SIZE,
+        output_dim=EMBED_SIZE,
+        input_length=INPUT_SIZE))
+    model.add(LSTM(
+        units=hp.Int("units", min_value=16, max_value=HIDDEN_SIZE, step=1),
+        dropout=DROPOUT, recurrent_dropout=REC_DROPOUT
+    ))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(
+        optimizer=Adam(
+            hp.Choice("learning_rate", values=[1e-2, 5e-3, 1e-3])
+        ),
+        loss="binary_crossentropy",
+        metrics=["accuracy"]
+    )
+    return model
 
 # vocab_size: size of vocabulary, also size of embedding input dimension
 # embed_size: number of dimensions in vector space to embed vocabulary into
@@ -11,8 +39,8 @@ def get_model(vocab_size, embed_size, input_length, hidden_size, dropout=0.0, re
     model = Sequential()
     model.add(Embedding(vocab_size, embed_size, input_length=input_length))
     model.add(LSTM(hidden_size, dropout=dropout, recurrent_dropout=recurrent_dropout))
-    model.add(Dense(64, activation='sigmoid'))
-    model.add(Dense(1))
+    # model.add(Dense(64, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
     return model
 
