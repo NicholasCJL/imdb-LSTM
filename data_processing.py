4ad13d1010c7733f6b9da31f25b933767d577760
Nicholas <chong.jiale.nicholas@gmail.com>
2021-10-10 16:40:39 +0800

    Added proper truncated vocab dict

--- data_processing.py
+++ data_processing.py
@@ -10,19 +10,45 @@ class DataSet():
         (self.x_train, self.y_train), (self.x_test, self.y_test) = imdb.load_data(num_words=num_words, seed=seed)
 
         # padding sequences to all be of the same length
-        self.x_train = pad_sequences(self.x_train, maxlen=maxlen)
-        self.x_test = pad_sequences(self.x_test, maxlen=maxlen)
+        self.x_train = pad_sequences(self.x_train, maxlen=maxlen, padding='post', truncating='post')
+        self.x_test = pad_sequences(self.x_test, maxlen=maxlen, padding='post', truncating='post')
 
         self.split_data(train_portion)
 
-        self.word_to_index = imdb.get_word_index()
+        self.word_index = imdb.get_word_index()
+        self.word_to_index = {word:id + 3 for word, id in self.word_index.items()}
+        self.word_to_index["[PAD]"] = 0
+        self.word_to_index["[START]"] = 1
+        self.word_to_index["[UNK]"] = 2
+        self.word_to_index["[UNUSED]"] = 3
         self.index_to_word = {i:word for (word, i) in self.word_to_index.items()}
+        self.form_vocab()
+
+    def form_vocab(self):
+        i2w_vocab = {}
+        w2i_vocab = {}
+
+        for sentence in self.x_train:
+            for id in sentence:
+                if id not in i2w_vocab:
+                    word = self.index_to_word[id]
+                    i2w_vocab[id] = word
+                    w2i_vocab[word] = id
+
+        for sentence in self.x_test:
+            for id in sentence:
+                if id not in i2w_vocab:
+                    word = self.index_to_word[id]
+                    i2w_vocab[id] = word
+                    w2i_vocab[word] = id
+        self.i2w_vocab = i2w_vocab
+        self.w2i_vocab = w2i_vocab
 
     def get_data(self):
         return (self.x_train, self.y_train), (self.x_test, self.y_test)
 
     def get_vocab_length(self):
-        return len(self.word_to_index)
+        return len(self.i2w_vocab)
 
     # splits data into ratio train:test -> (train_portion:1-train_portion)
     def split_data(self, train_portion):
