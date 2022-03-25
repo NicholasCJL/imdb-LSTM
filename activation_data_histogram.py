c72e602b52c7293554fbc64ae4059b4368ed9f99
Nicholas <chong.jiale.nicholas@gmail.com>
2022-03-25 16:03:52 +0800

    Modified code to allow for plotting based on length

--- activation_data_histogram.py
+++ activation_data_histogram.py
@@ -1,13 +1,27 @@
+# histogram of activation power spectrum gradients
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pickle
+import os
 from normalise_activations_individual import RegressionData
 
-with open("Results/timesteps150_embed32_hidden45_vocab4000/norm_activations_by_type_reg_stats/reg_data_all.pkl", 'rb') as file:
+min_length = 0
+max_length = 9999
+type = "trimmed_train"
+
+path = "timesteps500_embed32_hidden60_vocab4000_4"
+
+with open(f"Results/{path}/norm_activation_by_type_length/trimmed_train/power_fft_reg_data_train_trimmed_009.pkl", 'rb') as file:
     reg_data = pickle.load(file)
 
+# os.mkdir(f'Results/{path}/norm_activation_by_type_length/{type}')
+os.mkdir(f'Results/{path}/norm_activation_by_type_length/{type}/{min_length}-{max_length}')
+
 for data in reg_data:
-    x = np.asarray(data.slope)
+    x = data.slope
+    x_length = data.input_length
+    x = np.asarray([x[i] for i in range(len(x)) if (x_length[i] >= min_length and x_length[i] <= max_length)])
     print(len(x))
     validation = data.iscorrect
     x_correct = np.asarray([x[i] for i in range(len(x)) if validation[i]])
@@ -33,12 +47,12 @@ for data in reg_data:
     plt.axvline(mean+stddev, color = 'r', linestyle = '-.', linewidth=0.5)
     plt.hist(x, bins=bins, color='b')
     plt.xlim([-2, 2])
-    plt.xlabel("Gradient")
+    plt.xlabel(r"$\alpha$")
     plt.ylabel("Count")
-    plt.title(f'Spread of gradients of {data.type} across dataset')
-    plt.text(0.02, 0.9, f'Mean = {round(mean, 3)}\nMedian = {round(median, 3)}\nStddev = {round(stddev, 3)}',
+    plt.title(f'Spread of exponents of {data.type} across dataset')
+    plt.text(0.72, 0.9, f'Mean = {round(mean, 3)}\nMedian = {round(median, 3)}\nStddev = {round(stddev, 3)}',
              horizontalalignment='left', verticalalignment='center', transform=ax.transAxes)
-    plt.show()
+    fig.savefig(f"Results/{path}/norm_activation_by_type_length/{type}/{min_length}-{max_length}/{data.type}.png", dpi=fig.dpi)
 
     # correct data
     q25, q75 = np.percentile(x_correct, [0.25, 0.75])
@@ -57,12 +71,13 @@ for data in reg_data:
     plt.axvline(mean+stddev, color = 'r', linestyle = '-.', linewidth=0.5)
     plt.hist(x_correct, bins=bins, color='g')
     plt.xlim([-2, 2])
-    plt.xlabel("Gradient")
+    plt.xlabel(r"$\alpha$")
     plt.ylabel("Count")
     plt.title(f'Spread of gradients of {data.type} across dataset (correct predictions)')
-    plt.text(0.02, 0.9, f'Mean = {round(mean, 3)}\nMedian = {round(median, 3)}\nStddev = {round(stddev, 3)}',
+    plt.text(0.72, 0.9, f'Mean = {round(mean, 3)}\nMedian = {round(median, 3)}\nStddev = {round(stddev, 3)}',
              horizontalalignment='left', verticalalignment='center', transform=ax.transAxes)
-    plt.show()
+    fig.savefig(f"Results/{path}/norm_activation_by_type_length/{type}/{min_length}-{max_length}/{data.type}_c.png", dpi=fig.dpi)
+
 
     # wrong data
     q25, q75 = np.percentile(x_wrong, [0.25, 0.75])
@@ -81,9 +96,9 @@ for data in reg_data:
     plt.axvline(mean + stddev, color='r', linestyle='-.', linewidth=0.5)
     plt.hist(x_wrong, bins=bins, color='orange')
     plt.xlim([-2, 2])
-    plt.xlabel("Gradient")
+    plt.xlabel(r"$\alpha$")
     plt.ylabel("Count")
     plt.title(f'Spread of gradients of {data.type} across dataset (wrong predictions)')
-    plt.text(0.02, 0.9, f'Mean = {round(mean, 3)}\nMedian = {round(median, 3)}\nStddev = {round(stddev, 3)}',
+    plt.text(0.72, 0.9, f'Mean = {round(mean, 3)}\nMedian = {round(median, 3)}\nStddev = {round(stddev, 3)}',
              horizontalalignment='left', verticalalignment='center', transform=ax.transAxes)
-    plt.show()
+    fig.savefig(f"Results/{path}/norm_activation_by_type_length/{type}/{min_length}-{max_length}/{data.type}_w.png", dpi=fig.dpi)
